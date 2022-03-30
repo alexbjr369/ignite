467218877e39a835fec8d4cc633c377bc1b21892
alexbjr369 <alexbjr369@gmail.com>
2022-03-30 19:16:18 -0300

    commit

--- react-js/chapter-02/05-contextos-e-hooks/src/TransactionsContext.tsx
+++ react-js/chapter-02/05-contextos-e-hooks/src/TransactionsContext.tsx
@@ -12,7 +12,7 @@ interface Transaction {
 
 interface TransactionsContextData {
   transactions: Transaction[];
-  createTransaction: (transaction: TransactionInput) => void;
+  createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
 type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>;
@@ -34,8 +34,13 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
       .then((response) => setTransactions(response.data.transactions));
   }, []);
 
-  function createTransaction(transaction: TransactionInput) {
-    api.post('/transactions', transaction);
+  async function createTransaction(transactionInput: TransactionInput) {
+    const response = await api.post('/transactions', {
+      ...transactionInput,
+      createdAt: new Date(),
+    });
+    const { transaction } = response.data;
+    setTransactions([...transactions, transaction]);
   }
 
   return (
