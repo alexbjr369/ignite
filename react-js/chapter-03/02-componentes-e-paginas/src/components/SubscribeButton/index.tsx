d2e2bfa3aa8b62fc13546a5a0e7bfa72c8682708
alex-medgrupo <alexandre.junior@terceirizados.medgrupo.com.br>
2022-06-20 20:29:46 -0300

    commit

--- react-js/chapter-03/02-componentes-e-paginas/src/components/SubscribeButton/index.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './styles.module.scss';
-
-export function SubscribeButton() {
-  return (
-    <button className={styles.subscribeButton} type="button">
-      Subscribe now
-    </button>
-  );
-}
--- react-js/projects/dtmoney/README.md
+++
@@ -1,3 +0,0 @@
-# dtmoney
-
-Project developed on the course **[Ignite React.js](https://www.rocketseat.com.br/ignite)** by **[RocketSeat](https://www.rocketseat.com.br/)**.
