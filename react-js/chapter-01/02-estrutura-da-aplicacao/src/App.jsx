3601ec65aff48be2f02af1448770ef155ccf675f
alexbjr369 <alexbjr369@gmail.com>
2022-06-20 23:02:50 -0300

    commit

--- react-js/chapter-01/02-estrutura-da-aplicacao/src/App.jsx
+++ react-js/chapter-01/02-estrutura-da-aplicacao/src/App.jsx
@@ -1,8 +1,12 @@
+import { Header } from './components/Header';
 import { Post } from './Post';
+
+import './styles.css';
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post
         author="John Doe"
         content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis omnis totam nobis autem sequi. Alias?"
