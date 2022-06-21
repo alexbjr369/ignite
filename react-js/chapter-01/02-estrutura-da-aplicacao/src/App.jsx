b4fd265e127d1a7fc5f2dca82e344b4cc5e465a2
alexbjr369 <alexbjr369@gmail.com>
2022-06-21 13:33:33 -0300

    commit

--- react-js/chapter-01/02-estrutura-da-aplicacao/src/App.jsx
+++ react-js/chapter-01/02-estrutura-da-aplicacao/src/App.jsx
@@ -1,20 +1,27 @@
 import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
 import { Post } from './Post';
 
 import './global.css';
+import styles from './App.module.css';
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="John Doe"
-        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis omnis totam nobis autem sequi. Alias?"
-      />
-      <Post
-        author="John Doe 2"
-        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium dicta vero delectus quisquam aliquid aut."
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="John Doe"
+            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis omnis totam nobis autem sequi. Alias?"
+          />
+          <Post
+            author="John Doe 2"
+            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium dicta vero delectus quisquam aliquid aut."
+          />
+        </main>
+      </div>
     </div>
   );
 }
