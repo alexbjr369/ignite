bac7fb7d23648c5732a49fe7035c03ea450b7a6a
alexbjr369 <alexbjr369@gmail.com>
2022-06-20 22:37:14 -0300

    commit

--- react-js/chapter-01/01-iniciando-com-react/src/App.jsx
+++ react-js/chapter-01/01-iniciando-com-react/src/App.jsx
@@ -3,9 +3,14 @@ import { Post } from './Post';
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="John Doe"
+        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis omnis totam nobis autem sequi. Alias?"
+      />
+      <Post
+        author="John Doe 2"
+        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium dicta vero delectus quisquam aliquid aut."
+      />
     </div>
   );
 }
