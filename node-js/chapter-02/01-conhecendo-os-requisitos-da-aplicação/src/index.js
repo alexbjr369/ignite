4e22ee956eaca1de45229708675abdab23715454
alexbjr369 <alexbjr369@gmail.com>
2022-06-23 10:21:03 -0300

    commit

--- node-js/chapter-02/01-conhecendo-os-requisitos-da-aplicação/src/index.js
+++ node-js/chapter-02/01-conhecendo-os-requisitos-da-aplicação/src/index.js
@@ -17,9 +17,17 @@ app.post('/account', (request, response) => {
     return response.status(400).json({ error: 'Customer already exists' });
   }
 
-  customers.push({ cpf, name, id: uuidv4(), statment: [] });
+  customers.push({ cpf, name, id: uuidv4(), statement: [] });
 
   return response.status(201).send();
+});
+
+app.get('/statement/:cpf', (request, response) => {
+  const { cpf } = request.params;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  return response.json(customer.statement);
 });
 
 app.listen(3333);
