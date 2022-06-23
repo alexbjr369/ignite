9fac303bc0aebb4a42e6dac1c2eb39ae33ea6aed
alexbjr369 <alexbjr369@gmail.com>
2022-06-23 11:12:03 -0300

    commit

--- react-js/chapter-01/02-estrutura-da-aplicacao/src/components/Sidebar.jsx
+++ react-js/chapter-01/02-estrutura-da-aplicacao/src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react';
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -10,12 +11,19 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://avatars.githubusercontent.com/u/85613172?v=4"
+        />
         <strong>John Doe</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size="20" />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
