19766c0c9a34bc5fb133a9be153a3e0dc3cebe28
alex-medgrupo <alexandre.junior@terceirizados.medgrupo.com.br>
2022-06-25 20:30:22 -0300

    commit

--- react-js/chapter-01/02-estrutura-da-aplicacao/src/components/Sidebar.jsx
+++ react-js/chapter-01/02-estrutura-da-aplicacao/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -11,10 +12,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://avatars.githubusercontent.com/u/85613172?v=4"
-        />
+        <Avatar src="https://avatars.githubusercontent.com/u/85613172?v=4" />
         <strong>John Doe</strong>
         <span>Web Developer</span>
       </div>
