d5f693f1d23c778079002615082b97e6a8b29092
alex-medgrupo <alexandre.junior@terceirizados.medgrupo.com.br>
2022-06-25 18:28:23 -0300

    commit

--- react-js/chapter-01/02-estrutura-da-aplicacao/src/components/Post.jsx
+++ react-js/chapter-01/02-estrutura-da-aplicacao/src/components/Post.jsx
@@ -32,6 +32,14 @@ export function Post() {
           <a href="">#Lorem</a> <a href="">#Lorem</a> <a href="">#Lorem</a>
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comentário" />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   );
 }
