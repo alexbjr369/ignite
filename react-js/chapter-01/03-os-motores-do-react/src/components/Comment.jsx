90a266b6a64585a454baaa4e5abfe41f5f24dadc
alexbjr369 <alexbjr369@gmail.com>
2022-06-26 14:36:38 -0300

    commit

---
+++ react-js/chapter-01/03-os-motores-do-react/src/components/Comment.jsx
@@ -0,0 +1,40 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <Avatar
+        hasBorder={false}
+        src="https://avatars.githubusercontent.com/u/85613172?v=4"
+      />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>John Doe</strong>
+              <time
+                title="01 de julho às 08:00h"
+                dateTime="2022-07-01 08:00:00"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
---
+++ react-js/chapter-01/03-os-motores-do-react/src/components/Post.jsx
@@ -0,0 +1,50 @@
+import { Avatar } from './Avatar';
+import { Comment } from './Comment';
+import styles from './Post.module.css';
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <Avatar src="https://avatars.githubusercontent.com/u/85613172?v=4" />
+          <div className={styles.authorInfo}>
+            <strong>John Doe</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time title="01 de julho às 08:00h" dateTime="2022-07-01 08:00:00">
+          Publicado a 1h
+        </time>
+      </header>
+      <div className={styles.content}>
+        <p>Lorem, ipsum.</p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse autem
+          excepturi qui, laborum, alias exercitationem assumenda praesentium
+          eligendi suscipit fuga commodi nostrum minus dolores itaque.
+        </p>
+        <p>
+          Link: <a href="">Lorem, ipsum dolor.</a>
+        </p>
+        <p>
+          <a href="">#Lorem</a> <a href="">#Lorem</a> <a href="">#Lorem</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comentário" />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
+    </article>
+  );
+}
