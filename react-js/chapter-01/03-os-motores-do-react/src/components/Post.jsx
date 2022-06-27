62dbccef14bdb24f68a789f2f5c7de51e7143a77
alexbjr369 <alexbjr369@gmail.com>
2022-06-27 05:53:39 -0300

    commit

--- react-js/chapter-01/03-os-motores-do-react/src/components/Post.jsx
+++ react-js/chapter-01/03-os-motores-do-react/src/components/Post.jsx
@@ -1,35 +1,50 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
-export function Post() {
+export function Post({ author, publishedAt, content }) {
+  const publishedDateFormatted = format(
+    publishedAt,
+    "d 'de' LLLL 'às' HH:mm'h'",
+    { locale: ptBR }
+  );
+
+  const publishedDaterelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  });
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://avatars.githubusercontent.com/u/85613172?v=4" />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>John Doe</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
-        <time title="01 de julho às 08:00h" dateTime="2022-07-01 08:00:00">
-          Publicado a 1h
+        <time
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
+          {publishedDaterelativeToNow}
         </time>
       </header>
       <div className={styles.content}>
-        <p>Lorem, ipsum.</p>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse autem
-          excepturi qui, laborum, alias exercitationem assumenda praesentium
-          eligendi suscipit fuga commodi nostrum minus dolores itaque.
-        </p>
-        <p>
-          Link: <a href="">Lorem, ipsum dolor.</a>
-        </p>
-        <p>
-          <a href="">#Lorem</a> <a href="">#Lorem</a> <a href="">#Lorem</a>
-        </p>
+        {content.map((line) => {
+          if (line.type === 'paragraph') {
+            return <p>{line.content}</p>;
+          } else if (line.type === 'link') {
+            return (
+              <p>
+                <a href="#">{line.content}</a>
+              </p>
+            );
+          }
+        })}
       </div>
 
       <form className={styles.commentForm}>
