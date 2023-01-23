262a377483ae613ee7b2bc8cddbfcc46606d2a7a
alexbjr369 <alexbjr369@gmail.com>
2023-01-23 10:48:13 -0300

    commit

---
+++ react-native/2022/chapter-01/02-fundamentos/App.tsx
@@ -0,0 +1,32 @@
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#131016',
+        padding: 24,
+      }}
+    >
+      <Text
+        style={{
+          color: '#fdfcfe',
+          fontSize: 24,
+          fontWeight: 'bold',
+          marginTop: 48,
+        }}
+      >
+        Nome do evento
+      </Text>
+      <Text
+        style={{
+          color: '#6b6b6b',
+          fontSize: 16,
+        }}
+      >
+        Sexta, 4 de Novembro de 2022.
+      </Text>
+    </View>
+  );
+}
