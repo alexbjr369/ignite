c1b94afa6c2d3d1f57b28580a19ce93b63267bc1
alexbjr369 <alexbjr369@gmail.com>
2023-01-26 10:21:34 -0300

    commit

--- node-js/2023/chapter-01/05-rotas-da-aplicacao/src/server.js
+++ node-js/2023/chapter-01/05-rotas-da-aplicacao/src/server.js
@@ -3,12 +3,6 @@ import http from 'node:http';
 import { json } from './middlewares/json.js';
 import { routes } from './routes.js';
 
-// 1. query parameters
-// parâmetros nomeados que enviamos na requisição (http://localhost:333/users?userId=1&name=John)
-//
-// 2. route parameters:
-// 3. request body:
-
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
